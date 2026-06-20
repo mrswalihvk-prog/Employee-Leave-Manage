131bc3203a47dabaa3e9508ebcd8f9f935985d24
muhammedsinank881-code <muhammedsinank881@gmail.com>
2026-06-20 10:48:01 +0530

    updated version

--- Employee Leave Management/Backend/Routes/authRoutes.js
+++ Employee Leave Management/Backend/Routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { login ,registerUser} from "../controllers/authController.js";
 
 const router = express.Router()
 
 router.post("/login", login)
+router.post("/registerUser", registerUser)
 
-export default router
+export default router   
--- Employee Leave Management/Backend/controllers/authController.js
+++ Employee Leave Management/Backend/controllers/authController.js
@@ -1,6 +1,44 @@
 import Employee from "../models/Employee.js"
 import bcrypt from "bcrypt"
 
+
+
+
+ export const registerUser = async () => {
+  try {
+    
+
+    const adminExists = await Employee.findOne({
+      email: "admin@gmail.com",
+    });
+
+    if (adminExists) {
+      console.log("Admin already exists");
+      process.exit(0);
+    }
+
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+
+    await Employee.create({
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: hashedPassword,
+      phone: "9999999999",
+      department: "IT",
+      joinDate: new Date(),
+      salary: 50000,
+      role: "admin",
+    });
+
+    console.log("Admin seeded successfully");
+    process.exit(0);
+  } catch (error) {
+    console.error("Seed Error:", error.message);
+    process.exit(1);
+  }
+};
+
+
 export const login = async (req, res) => {
 
   try {
